833949b428023f893876933c93c1642d490182e0
bigbulse <blaise.notter@mymail.champlain.edu>
2021-03-08 11:02:14 -0500

    Comitted

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#Secure-ssh.sh
+#author Blaise
+#creates a new ssh user using $1 parameter
+#adds a public key from local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "this is where the code would go if I knew what this was meant to do"
