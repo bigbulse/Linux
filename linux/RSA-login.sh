85890faaf80ac36c7180414c4990f5dc3f75a671
bigbulse <blaise.notter@mymail.champlain.edu>
2021-03-09 14:38:34 -0500

    Working file

--- linux/RSA-login.sh
+++ linux/RSA-login.sh
@@ -1,6 +1,6 @@
-useradd -m -d /home/$1 -s /bin/bash $1
-mkdir /home/$1/.ssh
-cp /linux/public_keys/id_rsa.pub /home/$1/.ssh/authorized_keys
-chmod 700 /home/$1/.ssh
-chmod 600 /home/$1/.ssh/authorized_keys
-chown -R $1:$1 /home/$1/.ssh
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
